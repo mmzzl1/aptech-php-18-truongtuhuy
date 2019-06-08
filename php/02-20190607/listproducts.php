8abe3f684acf7568f80a86e4c86e9763bae0145a
mmzzl1 <truongtuhuy2@gmail.com>
2019-06-08 15:08:10 +0700

    add php folder

---
+++ php/02-20190607/listproducts.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<?php 
+	$products = [
+		["iphone", 999],
+		["ipad", 1000],
+		["xiaomi", 100],
+		["fpt", 40]	
+	]
+ ?>
+ <table class="table">
+ 	<tr>
+ 		<th>id</th>
+ 		<th>name</th>
+ 		<th>price</th>
+ 	</tr>
+ 	<?php
+ 		$total = 0;
+ 		for ($i = 0; $i < count($products) ; $i++) {
+ 			//tang thue 
+ 			$lastPrice = addTax($products,$i, 100);
+ 			echo "<tr>
+				<td>".( $i + 1 )."</td>
+				<td>".$products[$i][0]."</td>
+				<td>$".$lastPrice."</td>
+ 			</tr>";
+ 			$total += $lastPrice;
+ 		}
+ 		echo 
+ 			"<td>Total</td>
+ 			<td></td>
+			<td>".$total."</td>";
+ //function tang thue 10% cac san phan > 500$
+function addTax($products, $i, $value){
+		if ($products[$i][1] > $value) {
+			return $products[$i][1] + $products[$i][1] *10/100;
+		}else {
+			return $products[$i][1] +$products[$i][1] *5/100;
+		}
+}
+		 	 ?>
+ </table>
+</body>
+</html>
